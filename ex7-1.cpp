e5405cfe3668161eef2e150fc115701c2698bc64
11011055 <113594473+11011055@users.noreply.github.com>
2022-11-03 19:48:21 +0800

    Create ex7-1.cpp

---
+++ ex7-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a,i; //a =60
+   printf("please input a number:");
+   scanf ("%d",&a);
+   for(int i=0;i<60;i++){
+      if(60 % (i+1) ==0)
+      printf (" , %d ",(i+1));
+      }
+     printf ("\n");
+      
+    cout<<"Hello World";
+
+    return 0;
+}
