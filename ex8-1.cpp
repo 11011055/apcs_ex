3f7caf333c70be469e991529f7f78edbaa1ae7cd
11011055 <113594473+11011055@users.noreply.github.com>
2022-11-10 19:32:07 +0800

    Create ex8-1.cpp

---
+++ ex8-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int i,sum;
+    for(i=1;i<1000;i+=2){
+    int sum = sum + i;
+   printf("%d %d\n",i,sum);
+   
+}
+    return 0;
+}
