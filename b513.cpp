4c257022e86ca18ed3c75a9af806e9eb25b0d7ae
11011055 <113594473+11011055@users.noreply.github.com>
2022-12-29 20:27:05 +0800

    Create b513.cpp

---
+++ b513.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void is_prime(int num){
+     int i;
+     int not_prime  = 0;
+     for(i=2;i<num/2;i+=2){
+    
+        if(num % i==0){
+            printf("N");
+            not_prime = 1;
+            break;
+        }
+    }
+    if(not_prime != 1){
+            printf("Y");
+    } 
+  
+}
+
+
+int main(void){
+ int i, n,num;
+    //("please input a number");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&num);
+        is_prime(num);
+    }
+    return 0;
+}
